fd37b97b98c52451bcfbb3c4c863b6b277e387cb
chrisb-idox <chris.brighouse@idoxgroup.com>
2025-12-06 15:41:08 +0000

    perf: Add manual chunking for vendor dependencies in Vite build.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-mantine': ['@mantine/core', '@mantine/hooks', '@mantine/form'],
+          'vendor-dnd': ['@dnd-kit/core', '@dnd-kit/sortable', '@dnd-kit/utilities'],
+          'vendor-utils': ['react-hook-form'],
+        },
+      },
+    },
+  },
 })
